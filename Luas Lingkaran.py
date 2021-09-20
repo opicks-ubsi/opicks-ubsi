71967219a0cb23486ecf218bab5a2e3d582812f5
opicks-ubsi <91044500+opicks-ubsi@users.noreply.github.com>
2021-09-20 11:47:08 +0700

    Add files via upload

---
+++ Luas Lingkaran.py
@@ -0,0 +1,17 @@
+# Luas Lingkaran
+phi = 3.14
+r = float(input("Masukkan panjang jari-jari lingkaran: "))
+luas = phi*r*r
+print("Luas lingkaran adalah : "+ str(luas))
+
+# Luas Persegi Panjang
+p = float(input("Masukkan panjang : "))
+l = float(input("Masukkan lebar   : "))
+persegi = p*l
+print("Luas lingkaran adalah : "+ str(persegi))
+
+# Luas Bujur Sangkar
+#s = float(input("Masukkan sisi persegi / Bujur sangkar :"12))
+#luas = s*s
+
+#print("Luas Persegi / Bujur sangkar adalah : ",luas))
